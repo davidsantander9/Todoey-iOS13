5742e428a8c1ec59a8ca5744a01b65adc5b484a1
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-11-10 11:15:14 -0600

    table view methods and accessories

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -29,8 +29,20 @@ extension TodoListViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+    
+    
 }
 
