e1ae1122eb07caed94bf4f966da36d083df698e0
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-11-13 13:42:17 -0600

    save data with core data

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController{
     
@@ -18,6 +19,7 @@ class TodoListViewController: UITableViewController{
     
     let defaults = UserDefaults.standard
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +37,10 @@ class TodoListViewController: UITableViewController{
         let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default){ (action) in
-            let newItem = Item()
+            
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -54,10 +58,8 @@ class TodoListViewController: UITableViewController{
     
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
         do{
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: self.dataFilePath!)
+            try context.save()
         }catch{
             print("Error \(error)")
         }
@@ -65,16 +67,14 @@ class TodoListViewController: UITableViewController{
     }
     
     func loadItems(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch{
-                print("error \(error )")
-            }
+     
+        do{
+                
+        }catch{
+            print("error \(error )")
         }
-        
     }
+        
 }
 
 extension TodoListViewController {
