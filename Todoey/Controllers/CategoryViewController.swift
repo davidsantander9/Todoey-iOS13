4608f6f5acfbb0bda07807087285a5091ae5ffac
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-11-29 12:21:13 -0600

    setup categoryViewController

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,36 +7,79 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tittle = "Todo App"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = "Todo App"
+    }
+    
+    // MARK: - Data manipulation methods
+    
+    func saveCategories(){
+        do{
+            try context.save()
+        } catch {
+            print("Error saving category \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadCategories(){
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do{
+            categories = try context.fetch(request)
+        }catch{
+            print("Error loading categories")
+        }
+        
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
     
     
     @IBAction func addButtonPressed(_ sender: Any) {
         
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default){ (action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveCategories()
+            
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a category"
+        }
+        
+        present(alert, animated: true, completion: nil)
     }
     
 
 }
+
